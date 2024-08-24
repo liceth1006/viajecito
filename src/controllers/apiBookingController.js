bb2d2678b3cda27f745b6ef125fc119602fd0621
liceth1006 <olmos.1006@gmail.com>
2024-08-24 13:59:10 -0500

    hotel

--- src/controllers/apiBookingController.js
+++ src/controllers/apiBookingController.js
@@ -1,53 +1,75 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 
-const url = 'https://booking-com.p.rapidapi.com/v2/hotels/search?dest_id=-553173&order_by=popularity&checkout_date=2025-01-19&children_number=2&filter_by_currency=AED&locale=en-gb&dest_type=city&checkin_date=2025-01-18&children_ages=5,0&include_adjacency=true&page_number=0&adults_number=2&room_number=1&units=metric';
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'x-rapidapi-key': '6f00897639msh86987678ca2a162p198f92jsn361500abf0d8',
-    'x-rapidapi-host': 'booking-com.p.rapidapi.com'
+    "x-rapidapi-key": "6f00897639msh86987678ca2a162p198f92jsn361500abf0d8",
+    "x-rapidapi-host": "booking-com.p.rapidapi.com",
+  },
+};
+
+export const searchDestination = async (req, res) => {
+  const name = "colombia"
+  const URL_SEARCH_DES =`https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=es&name=${name}`
+  try {
+    const response = await axios.get(URL_SEARCH_DES, {
+      headers: options.headers,
+    });
+    const result = response.data;
+    console.log(result)
+    res.render("index", { items: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
   }
 };
 
+
+
 export const getApiBooking = async (req, res) => {
+  const {orderBy} = req.query;
+  const order_By = orderBy || "popularity";
+  
+  const URL_SEARCH_HOTEL = `https://booking-com.p.rapidapi.com/v1/hotels/search?children_ages=5%2C0&adults_number=2&children_number=2&room_number=1&include_adjacency=true&units=metric&checkout_date=2025-01-19&dest_id=47&filter_by_currency=COP&dest_type=country&checkin_date=2025-01-18&order_by=${order_By}&locale=es`
   try {
-    const response = await axios.get(url, { headers: options.headers });
-    const result = response.data.results;
-    if (req.path === '/') {
-      res.render('index', { items: result });
-    } else if (req.path === '/hotel') {
-      res.render('hotel/hotel', { items: result });
+    const response = await axios.get(URL_SEARCH_HOTEL, { headers: options.headers });
+    const result = response.data.result;
+    console.log("este es",orderBy)
+    if (req.path === "/") {
+      res.render("index", { items: result });
+    } else if (req.path === "/hotel") {
+      res.render("hotel/hotel", { items: result });
     } else {
-      res.status(404).send('Página no encontrada');
+      res.status(404).send("Página no encontrada");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('An error occurred');
+    res.status(500).send("An error occurred");
   }
 };
 
+
+
+
+
 export const detailsApiBooking = async (req, res) => {
-  const hotelId = req.params.id; 
+  const hotelId = req.params.id;
 
   try {
     const response = await axios.get(url, { headers: options.headers });
     const result = response.data.results;
 
     // Filtrar los detalles del hotel por ID
-    const hotelDetails = result.find(hotel => hotel.id == hotelId);
-    console.log("este es el hotel!",hotelDetails);
+    const hotelDetails = result.find((hotel) => hotel.id == hotelId);
     // Verificar si se encontró el hotel
     if (hotelDetails) {
-      console.log("este es el hotel!",hotelDetails);
       // Renderizar la vista desde la carpeta 'hotel'
-      res.render('hotel/hotelDetails', { item: hotelDetails });
+      res.render("hotel/hotelDetails", { item: hotelDetails });
     } else {
-      res.status(404).send('Hotel no encontrado');
+      res.status(404).send("Hotel no encontrado");
     }
   } catch (error) {
     console.error(error);
-    res.status(500).send('An error occurred');
+    res.status(500).send("An error occurred");
   }
 };
-
-
