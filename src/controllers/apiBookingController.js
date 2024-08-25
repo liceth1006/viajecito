b2a05cbf1b37ead3ff3f212aada9888f44f73591
liceth1006 <olmos.1006@gmail.com>
2024-08-24 21:37:54 -0500

    hotel

--- src/controllers/apiBookingController.js
+++ src/controllers/apiBookingController.js
@@ -3,20 +3,20 @@ import axios, { Axios } from "axios";
 const options = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "6f00897639msh86987678ca2a162p198f92jsn361500abf0d8",
+    "x-rapidapi-key": "3211edd23cmsh487d539589a2ce0p13146fjsn24af5d630ce6",
     "x-rapidapi-host": "booking-com.p.rapidapi.com",
   },
 };
 
 export const searchDestination = async (req, res) => {
-  const name = "colombia"
-  const URL_SEARCH_DES =`https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=es&name=${name}`
+  const { name } = req.query;
+  const nameLocation = name || "colombia";
+  const URL_SEARCH_DES = `https://booking-com.p.rapidapi.com/v1/hotels/locations?locale=es&name=${nameLocation}`;
   try {
     const response = await axios.get(URL_SEARCH_DES, {
       headers: options.headers,
     });
     const result = response.data;
-    console.log(result)
     res.render("index", { items: result });
   } catch (error) {
     console.error(error);
@@ -24,17 +24,43 @@ export const searchDestination = async (req, res) => {
   }
 };
 
-
-
 export const getApiBooking = async (req, res) => {
-  const {orderBy} = req.query;
+  const {
+    orderBy,
+    checkin_date,
+    checkout_date,
+    room_number,
+    children_number,
+    adults_number,
+  } = req.query;
+
   const order_By = orderBy || "popularity";
-  
-  const URL_SEARCH_HOTEL = `https://booking-com.p.rapidapi.com/v1/hotels/search?children_ages=5%2C0&adults_number=2&children_number=2&room_number=1&include_adjacency=true&units=metric&checkout_date=2025-01-19&dest_id=47&filter_by_currency=COP&dest_type=country&checkin_date=2025-01-18&order_by=${order_By}&locale=es`
+  const children = children_number || "2";
+  const adult = adults_number || "2";
+  const room = room_number || "1";
+
+  // Obtener la fecha de hoy y formatearla como 'YYYY-MM-DD'
+  const today = new Date();
+  const formattedDate = today.toISOString().split("T")[0];
+
+  // Crear la fecha de checkout sumando un día a checkin_date
+  const checkinDateObj = new Date(formattedDate);
+  checkinDateObj.setDate(checkinDateObj.getDate() + 1);
+  const formattedCheckoutDate =
+    checkout_date || checkinDateObj.toISOString().split("T")[0];
+
+  const checkin = checkin_date || formattedDate;
+  const checkout = checkout_date || formattedCheckoutDate;
+  const dest_type = "country" || "city";
+  const dest_id = "47";
+
+  const URL_SEARCH_HOTEL = `https://booking-com.p.rapidapi.com/v1/hotels/search?&adults_number=${adult}&children_number=${children}&room_number=${room}&include_adjacency=true&units=metric&checkout_date=${checkout}&dest_id=47&filter_by_currency=COP&dest_type=${dest_type}&checkin_date=${checkin}&order_by=${order_By}&locale=es`;
+
   try {
-    const response = await axios.get(URL_SEARCH_HOTEL, { headers: options.headers });
+    const response = await axios.get(URL_SEARCH_HOTEL, {
+      headers: options.headers,
+    });
     const result = response.data.result;
-    console.log("este es",orderBy)
     if (req.path === "/") {
       res.render("index", { items: result });
     } else if (req.path === "/hotel") {
@@ -50,24 +76,60 @@ export const getApiBooking = async (req, res) => {
 
 
 
-
+const retryRequest = async (url, options, retries = 3) => {
+  let attempt = 0;
+  while (attempt < retries) {
+    try {
+      return await axios.get(url, options);
+    } catch (error) {
+      if (error.response && error.response.status === 429) { // Código de estado 429: Demasiadas Solicitudes
+        attempt++;
+        const delay = Math.pow(2, attempt) * 1000; // Retardo exponencial
+        console.log(`Rate limit exceeded. Retrying in ${delay / 1000} seconds...`);
+        await new Promise(resolve => setTimeout(resolve, delay));
+      } else {
+        throw error; // Re-lanzar el error si no es un error de límite de tasa
+      }
+    }
+  }
+  throw new Error('Max retries exceeded');
+};
 
 export const detailsApiBooking = async (req, res) => {
-  const hotelId = req.params.id;
+  const hotelId = req.params.hotel_id;
+
+  const URL_HOTEL_DETAILS = `https://booking-com.p.rapidapi.com/v1/hotels/data?hotel_id=${hotelId}&locale=es`;
+  const URL_HOTEL_PHOTO = `https://booking-com.p.rapidapi.com/v1/hotels/photos?hotel_id=${hotelId}&locale=es`;
 
   try {
-    const response = await axios.get(url, { headers: options.headers });
-    const result = response.data.results;
+    const [detailsResponse, photosResponse] = await Promise.all([
+      retryRequest(URL_HOTEL_DETAILS, { headers: options.headers }),
+      retryRequest(URL_HOTEL_PHOTO, { headers: options.headers })
+    ]);
 
-    // Filtrar los detalles del hotel por ID
-    const hotelDetails = result.find((hotel) => hotel.id == hotelId);
-    // Verificar si se encontró el hotel
-    if (hotelDetails) {
-      // Renderizar la vista desde la carpeta 'hotel'
-      res.render("hotel/hotelDetails", { item: hotelDetails });
-    } else {
-      res.status(404).send("Hotel no encontrado");
-    }
+    const details = detailsResponse.data;
+
+    // Buscar la descripción en español o tomar una descripción de cualquier idioma
+    const descriptionTranslations = details.description_translations || [];
+    const descriptionSpanish = descriptionTranslations.find(
+      desc => desc.languagecode === 'es'
+    )?.description || 'Descripción no disponible';
+
+
+    // Asegurarse de que photosResponse.data tiene al menos 3 elementos
+    const photosArray = photosResponse.data;
+    const photos1 = photosArray[0] || {};
+    const photos2 = photosArray[1] || {};
+    const photos3 = photosArray[2] || {};
+
+    console.log("foto",photos1)
+    res.render("hotel/hotelDetails", {
+      details,
+      photos1,
+      photos2,
+      photos3,
+      descriptionSpanish,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred");
--- src/routes/viajecito.routes.js
+++ src/routes/viajecito.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getApiBooking,detailsApiBooking,searchDestination } from '../controllers/apiBookingController.js';
+import { getApiBooking,searchDestination,detailsApiBooking} from '../controllers/apiBookingController.js';
 
 const router = express.Router();
 
 // Ruta para mostrar la página de inicio con los hoteles
 router.get("/", searchDestination);
 router.get("/hotel", getApiBooking);
-// router.get("/hotelDetails/:id", detailsApiBooking);
+router.get("/hotelDetails/:hotel_id", detailsApiBooking);
 
 
 export default router;
